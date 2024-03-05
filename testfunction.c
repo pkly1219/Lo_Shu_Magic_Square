b134789f856ee29aa14eb0a05501a84f9fdd5cde
Phung Ly <98497119+pkly1219@users.noreply.github.com>
2024-03-05 14:47:16 -0800

    Update testfunction.c

--- testfunction.c
+++ testfunction.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
 //sum of one row
 int sumOfRow (int const arr[][3], int num)
 {
@@ -14,6 +15,7 @@ int sumOfRow (int const arr[][3], int num)
     return sum;
 }
 
+
 //sum of one column
 int sumOfColumn (int const arr[][3], int num)
 {
@@ -25,18 +27,21 @@ int sumOfColumn (int const arr[][3], int num)
      return sum;
 }
 
+
 //sum of a diagonal line
 int sumOfDiagonalLine1 (int const arr[][3] )
 {
     return arr[0][0] + arr[1][1] + arr[2][2];
 }
 
+
 //sum of the other diagonal line
 int sumOfDiagonalLine2 (int const arr[][3] )
 {
     return arr[2][0] + arr[1][1] + arr[0][2];
 }
- 
+
+
  bool test(int const arr[][3])
  {
      int sumR0, sumR1, sumR2;
